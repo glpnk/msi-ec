6fef9cd1595f11f64fa92b2753af74b967e3552d
GlitchPunk <16731610+glpnk@users.noreply.github.com>
2024-05-23 11:16:37 +0300

    apply <6.2 patch

--- msi-ec.c
+++ msi-ec.c
@@ -46,6 +46,7 @@
 #include <linux/seq_file.h>
 #include <linux/string.h>
 #include <linux/slab.h>
+#include <linux/version.h>
 
 #define SM_ECO_NAME		"eco"
 #define SM_COMFORT_NAME		"comfort"
@@ -1903,14 +1904,22 @@ static struct attribute *msi_battery_attrs[] = {
 
 ATTRIBUTE_GROUPS(msi_battery);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,0)
 static int msi_battery_add(struct power_supply *battery,
 			   struct acpi_battery_hook *hook)
+#else
+static int msi_battery_add(struct power_supply *battery)
+#endif
 {
 	return device_add_groups(&battery->dev, msi_battery_groups);
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,0)
 static int msi_battery_remove(struct power_supply *battery,
 			      struct acpi_battery_hook *hook)
+#else
+static int msi_battery_remove(struct power_supply *battery)
+#endif
 {
 	device_remove_groups(&battery->dev, msi_battery_groups);
 	return 0;
