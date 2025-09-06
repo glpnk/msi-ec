56e7ffe9874b8745749374bd831b7d818cde7bdd
GlitchPunk <16731610+glpnk@users.noreply.github.com>
2025-09-06 04:51:14 +0300

    del 50 move to 10

--- msi-ec.c
+++ msi-ec.c
@@ -765,6 +765,9 @@ static struct msi_ec_conf CONF9 __initdata = {
 static const char *ALLOWED_FW_10[] __initconst = {
 	"1581EMS1.107", // Katana GF66 11UE / 11UG
 	"1582EMS1.107", // Katana GF66 11UC / 11UD
+	"1583EMS1.110", // Pulse  GL66 12UGK / Crosshair 15 B12UEZ / Katana GF66-12UG
+	"1584EMS1.104", // Katana GF66 12UD
+	"1584EMS1.112", // Katana GF66 12UC
 	NULL
 };
 
@@ -790,7 +793,6 @@ static struct msi_ec_conf CONF10 __initdata = {
 		.modes = {
 			{ SM_ECO_NAME,     0xc2 },
 			{ SM_COMFORT_NAME, 0xc1 },
-			{ SM_SPORT_NAME,   0xc0 },
 			{ SM_TURBO_NAME,   0xc4 },
 			MSI_EC_MODE_NULL
 		},
@@ -3250,75 +3252,6 @@ static struct msi_ec_conf CONF49 __initdata = {
 	},
 };
 
-static const char *ALLOWED_FW_50[] __initconst = {
-	"1584EMS1.104",// Katana GF66 12UD
-	"1584EMS1.112", // Katana GF66 12UC
-	"1583EMS1.110", // Pulse  GL66 12UGK / Crosshair 15 B12UEZ / Katana GF66-12UG
-	NULL
-};
-
-static struct msi_ec_conf CONF50 __initdata = {
-	.allowed_fw = ALLOWED_FW_50, // WMI2 based
-	.charge_control_address = 0xd7,
-	.webcam = {
-		.address       = 0x2e,
-		.block_address = 0x2f,
-		.bit           = 1,
-	},
-	.fn_win_swap = {
-		.address = 0xe8,
-		.bit     = 4,
-		.invert  = true,
-	},
-	.cooler_boost = {
-		.address = 0x98,
-		.bit     = 7,
-	},
-	.shift_mode = {
-		.address = 0xd2,
-		.modes = {
-			{ SM_ECO_NAME,     0xc2 }, // super battery (D4=0D)
-			{ SM_COMFORT_NAME, 0xc1 }, // balanced + silent
-			{ SM_TURBO_NAME,   0xc4 }, // extreme performance
-			MSI_EC_MODE_NULL
-		},
-	},
-	.super_battery = {
-		.address = 0xeb, // only in SB
-		.mask    = 0x0f,
-	},
-	.fan_mode = {
-		.address = 0xd4,
-		.modes = {
-			{ FM_AUTO_NAME,     0x0d },
-			{ FM_SILENT_NAME,   0x1d }, // only silent
-			{ FM_ADVANCED_NAME, 0x8d },
-			MSI_EC_MODE_NULL
-		},
-	},
-	.cpu = {
-		.rt_temp_address      = 0x68,
-		.rt_fan_speed_address = 0x71,
-	},
-	.gpu = {
-		.rt_temp_address      = 0x80,
-		.rt_fan_speed_address = 0x89,
-	},
-	.leds = {
-		.micmute_led_address = 0x2c,
-		.mute_led_address    = 0x2d,
-		.bit                 = 1,
-	},
-	.kbd_bl = {
-		.bl_mode_address  = MSI_EC_ADDR_UNSUPP,
-		.bl_modes         = { 0x00, 0x08 },
-		.max_mode         = 1,
-		.bl_state_address = 0xd3,
-		.state_base_value = 0x80,
-		.max_state        = 3,
-	},
-};
-
 static const char *ALLOWED_FW_51[] __initconst = {
 	"158MEMS1.100", // Bravo 15 B5ED
 	"158MEMS1.101",
@@ -3839,7 +3772,6 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF47,
 	&CONF48,
 	&CONF49,
-	&CONF50,
 	&CONF51,
 	&CONF53,
 	&CONF55,
