ee0cd29032cf5a7d50ae04409dd42732e1834bd0
Xabi GOITY <xabigoity@gmail.com>
2026-07-26 12:24:39 +0200

    Update msi-ec.c
    corrected one problem with ; on declaring Blacklist

--- msi-ec.c
+++ msi-ec.c
@@ -57,7 +57,7 @@ static DEFINE_MUTEX(ec_set_bit_mutex);
 //Used to ban specific firmware versions in case they are buggy.
 static const char *BANNED_FW[] __initconst = {
 	NULL
-}
+};
 
 
 /* **************** Gen 1 - WMI1 **************** */
